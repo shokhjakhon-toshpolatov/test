27a53cb1f98f1d260a6090bc4aeedb646164a359
Shohjahon <toshpolatovshohjahon2501@gmail.com>
2025-10-25 01:50:26 +0500

    Project Created!!!

---
+++ just.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    char hasMoney;
+    double amount;
+    cout << "How old are you? ";
+    cin >> age;
+
+    if (age >= 18){
+        cout << "Do you have money? [Y/N] ";
+        cin >> hasMoney;
+
+        if (hasMoney == 'Y' || hasMoney == 'y'){
+            cout << "how much money do you have? " << endl;
+            cin >> amount;
+
+            if (amount > 1300.0){
+                cout << "iPhone is yours";
+
+            }else if (amount < 1300.0 and amount >= 800.0){
+                cout << "Choose from our not-0Pro models";
+
+            } else {
+                cout << "How dare you come here!";
+
+            }
+        }else{
+            cout << "Borrow it";
+
+        }
+    }else {
+        cout << "Get the hell out of here";
+    }
+
+    return 0;
+}
---
+++ just.h
@@ -0,0 +1,13 @@
+//
+// Created by SHOHJAHON on 10/15/2025.
+//
+
+#ifndef UNTITLED1_JUST_H
+#define UNTITLED1_JUST_H
+
+
+class just {
+};
+
+
+#endif //UNTITLED1_JUST_H
